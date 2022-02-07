65b0b7dde3b36cce0e12894a5afb0d97246a98ce
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-02-07 13:06:17 +0600

    Time: 3 ms (78.44%), Space: 7 MB (9.15%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        map<char,int>s_oc;
+        map<char,int>t_oc;
+        int i,j,k,m,n;
+        for(i=0;i<s.size();i++) s_oc[s[i]]++;
+        for(i=0;i<t.size();i++) t_oc[t[i]]++;
+        char ans;
+        
+        for(auto it:t_oc)
+        {
+            if(s_oc[it.first]!=it.second)
+            {
+                ans=it.first;
+                break;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
