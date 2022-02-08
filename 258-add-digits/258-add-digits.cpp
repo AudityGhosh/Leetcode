1b41c4816e08e65e95c67d627fae1551c750f18d
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-02-08 11:43:21 +0600

    Time: 5 ms (17.94%), Space: 6 MB (25.37%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
     int addDigits(int num) {
+        //Without Recursion or loops, try writing testcases with pen and paper
        if(num==0) return 0;
         if(num%9==0) return 9;
         else return num%9;
+        /**A    U   D   I   T   Y       G   H   O   S   H**/
     }
 };
