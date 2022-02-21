c1f4c6384ebce1cfdf098dda2c0a7a8efe42b23a
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-02-21 09:44:55 +0600

    Time: 16 ms (91.30%), Space: 19.6 MB (88.96%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        map<int,int>occ;
-        int i,j,k,m,n,t;
-        for(i=0;i<nums.size();i++) occ[nums[i]]++;
+        sort(nums.begin(),nums.end());
         
-        int ans=-1;
-        for(auto it:occ)
-        {
-            if(it.second>(nums.size()/2))
-            {
-                ans=it.first;
-                break;
-            }
-        }
-        
-        return ans;
+        return nums[nums.size()/2];
         
     }
 };
