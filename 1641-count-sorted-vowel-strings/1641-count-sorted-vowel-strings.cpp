e80d17b0b7cc6c29aa08a7ef7bdc776d74d48577
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-05-11 19:02:24 +0600

    Time: 0 ms (100.00%), Space: 5.8 MB (85.48%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //if we closely observe the corresponding values of the count with respect to n, we will find that it is a series of pentatope numbers, details from GFG https://www.geeksforgeeks.org/pentatope-number/
     int countVowelStrings(int n) {
         n++;
         return n * (n + 1) * (n + 2) * (n + 3) / 24;
