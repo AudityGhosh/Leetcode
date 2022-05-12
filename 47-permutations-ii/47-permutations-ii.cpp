876ea698fb33e9a6759897c4df8f821cf101b75f
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-05-12 11:16:10 +0600

    Time: 3 ms (97.10%), Space: 8.3 MB (98.84%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.cpp
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(),nums.end()); //if we sort it the first permutation will be smallest, and we will collect all permutations using next permutation on that. And yes, next permutation works on array as well.
         vector<vector<int>>ans;
         do {
        ans.push_back(nums);
