f8a3defdb6dcaf260bc860f29ac44006314da551
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-07-12 12:45:26 +0600

    Time: 3 ms (36.07%), Space: 5.9 MB (27.01%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/371-sum-of-two-integers.cpp
@@ -4,19 +4,22 @@ public:
         
         int ans=a;
         
-        if (b > 0)
+        if (b > 0)      // try adding 8 + 5, or -8 + 5
         {
           for(int i=0;i<b;i++) 
               ans++; 
         }
         
-        if (b < 0) 
+        if (b < 0)      // try adding 8 - 5, or -8 - 5
         { 
            for(int i=b;i<0;i++)
                ans--;
         }
+         
+        return ans;       // if b is zero, ans is a
         
-        return ans;
+        
+        /*AUDITY GHOSH*/
     
     
         
