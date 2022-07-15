4a917b0eb3f4c040953159c887a4892170a3c01a
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-07-15 11:18:46 +0600

    Time: 56 ms (36.44%), Space: 13.8 MB (49.51%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+                
+        n = bin(n)       #converts decimal to binary
+        n = n.replace('0b','')  #provides a string with "0b" at beginning, remove it
+        
+        #The zfill() method adds zeros (0) at the beginning of the string, until it reaches the specified length.
+        n = n.zfill(32)      
+
+        n = n[::-1] #reverse the string
+
+        
+
+        return int(n,2)  #convert binary string to decimal
+        
