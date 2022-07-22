03db8acb6f549b61e6c665e019380888196664ae
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2022-07-22 10:50:12 +0600

    Time: 4 ms (98.87%), Space: 10.4 MB (67.38%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& arr) {
+vector<int> ans;
+int n = arr.size();
+for (int i = 0; i < n; i++) {
+    auto it
+        = lower_bound(ans.begin(), ans.end(), arr[i]);
+    if (it == ans.end()) {
+        ans.push_back(arr[i]);
+        //cout<<arr[i]<<endl;
+    }
+    else {
+        *it = arr[i];
+    }
+}
+return ans.size();
+        
+    }
+};
+/* helpful to understand the intutuion:
+https://www.geeksforgeeks.org/c-program-for-longest-increasing-subsequence -> method 2
+in detail intution: https://www.youtube.com/watch?v=on2hvxBXJH4
+*/
