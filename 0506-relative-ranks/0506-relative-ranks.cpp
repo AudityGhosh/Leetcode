602cad0f4365c333f1af10a1624ff2242024889e
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2024-05-08 11:11:10 +0600

    Time: 3 ms (96.69%), Space: 14.4 MB (55.66%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
+        int N = score.size();
+        vector<int> scoreCopy(N);
+        copy(score.begin(), score.end(), scoreCopy.begin()); //copy the vector into a new vector
         
-       map<int,string>score_pos;
-        
-        int i,j,k,m,n,t;
-        
-        for(i=0;i<score.size();i++)
-        {
-            score_pos[score[i]]="0";
+        // Save the index of each athlete
+        unordered_map<int, int> scoreToIndex; 
+        for (int i = 0; i < N; i++) {
+            scoreToIndex[scoreCopy[i]] = i;
         }
         
-        map<int,string>score_real_pos;
-        
-        t = score.size();
-        
-        for(auto it:score_pos)
-        {
-            string s = to_string(t);
-            if(s=="1") score_real_pos[it.first] = "Gold Medal";
-            else if(s=="2") score_real_pos[it.first] = "Silver Medal";
-            else if(s=="3") score_real_pos[it.first] = "Bronze Medal";
-            else score_real_pos[it.first] = s;
-            t=t-1;        
-                  
-            
-            
+        // Sort scoreCopy in descending order
+        sort(scoreCopy.begin(), scoreCopy.end(), greater<int>()); //inverse sort
+
+        // Assign ranks to athletes
+        vector<string> rank(N);
+        for (int i = 0; i < N; i++) {
+            if (i == 0) {
+                rank[scoreToIndex[scoreCopy[i]]] = "Gold Medal"; //scorecopy sorted, so or first index e 1st place e thakbe, otar index koto ? rank er oi index e gold medal boshao
+            } else if (i == 1) {
+                rank[scoreToIndex[scoreCopy[i]]] = "Silver Medal";
+            } else if (i == 2) {
+                rank[scoreToIndex[scoreCopy[i]]] = "Bronze Medal";
+            } else {
+                rank[scoreToIndex[scoreCopy[i]]] = to_string(i + 1);
+            }
         }
         
-        for(auto it:score_real_pos)
-            cout<<it.first<<" "<<it.second<<endl;
-        
-       
-        
-        
-        vector<string>ans(score.size(),"0");
-        
-        for(i=0;i<score.size();i++)
-            ans[i] = score_real_pos[score[i]];
-        
-        
-        return ans;
-        
-        
+        return rank;
     }
 };
