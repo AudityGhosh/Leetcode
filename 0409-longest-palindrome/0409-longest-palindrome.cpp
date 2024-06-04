565386acc9e86d92dc0e5eb1a947f0e1d065d13f
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2024-06-04 09:33:24 +0600

    Time: 3 ms (55.35%), Space: 8.9 MB (62.59%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int i,j,k,m,n,t;
+        
+        map<char,int>freq;
+        
+        for(i=0;i<s.size();i++)
+        {
+            freq[s[i]]++;
+
+        }
+        
+        int ans=0;
+        
+        int jora_jora = 0;
+        
+        for(auto it:freq)
+        {
+            if(it.second%2==0)
+            {
+                jora_jora+=it.second;
+                freq[it.first]=0;
+            }
+            else
+            {
+                jora_jora += (it.second-1);
+                freq[it.first]=1;
+            }
+        }
+        
+        
+        
+        for(auto it:freq)
+            cout<<it.first<<" "<<it.second<<endl;
+        
+        for(auto it:freq)
+        {
+            if(it.second==1)
+            {
+                jora_jora++;
+                break;
+            }
+        }
+        
+        ans=jora_jora;
+        
+        return ans;
+            
+        
+    }
+};
