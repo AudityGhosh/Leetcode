2f76f08a9f4d907b9c256a92f78ce8a99f81a113
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2024-06-18 10:40:20 +0600

    Time: 52 ms (54.38%), Space: 41.3 MB (47.29%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -15,6 +15,7 @@ public:
         for (int i = 0; i < worker.size(); i++) {
             // While the index has not reached the end and worker can pick a job
             // with greater difficulty move ahead.
+            // if there is not unique index of difficulty where 2 can be assigned for the first example, so the profit of 2 will be the profit of previous worker 
             while (index < difficulty.size() and
                    worker[i] >= jobProfile[index].first) {
                 maxProfit = max(maxProfit, jobProfile[index].second);
