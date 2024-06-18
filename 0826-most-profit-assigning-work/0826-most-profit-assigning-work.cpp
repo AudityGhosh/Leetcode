55c1b0a8e8eb67020cac19fb809789df7d84bb03
Audity Ghosh <78872839+AudityGhosh@users.noreply.github.com>
2024-06-18 10:50:22 +0600

    Time: 45 ms (79.38%), Space: 48.4 MB (16.24%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -2,27 +2,33 @@ class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit,
                             vector<int>& worker) {
-        vector<pair<int, int>> jobProfile;
-        for (int i = 0; i < difficulty.size(); i++) {
-            jobProfile.push_back({difficulty[i], profit[i]});
+        vector<int>jobs;
+        
+        int maxAbility = *max_element(begin(worker), end(worker)); 
+        jobs.assign(maxAbility+1,0);
+        
+        int ans=0,i,j,k,m,n,t;
+        
+      
+        
+        
+        
+        for(i=0;i<difficulty.size();i++)
+        {
+             if (difficulty[i] <= maxAbility)
+            jobs[difficulty[i]] = max(jobs[difficulty[i]],profit[i]);
         }
-
-        // Sort both worker and jobProfile arrays
-        sort(worker.begin(), worker.end());
-        sort(jobProfile.begin(), jobProfile.end());
-
-        int netProfit = 0, maxProfit = 0, index = 0;
-        for (int i = 0; i < worker.size(); i++) {
-            // While the index has not reached the end and worker can pick a job
-            // with greater difficulty move ahead.
-            // if there is not unique index of difficulty where 2 can be assigned for the first example, so the profit of 2 will be the profit of previous worker 
-            while (index < difficulty.size() and
-                   worker[i] >= jobProfile[index].first) {
-                maxProfit = max(maxProfit, jobProfile[index].second);
-                index++;
-            }
-            netProfit += maxProfit;
-        }
-        return netProfit;
+        
+        for(i=1;i<jobs.size();i++)
+            jobs[i] = max(jobs[i],jobs[i-1]);
+        
+        
+        for(i=0;i<worker.size();i++)
+            ans+=jobs[worker[i]];
+        
+        
+        
+        
+        return ans;
     }
 };
